c7642aed4f88f40050cc65354833059b67a3a7cb
Angel Emanuel Villalonga <88913610+Ema-villalonga@users.noreply.github.com>
2023-11-24 12:10:10 -0300

    The project product ecommerce

---
+++ src/components/Products.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import './Products.css';
+
+export default function Products({products}){
+    
+    const [buy, setBuy] = useState(false);
+
+    function handleChange(){
+        setBuy(true)
+    }
+
+    return (
+        <main className='products'>
+            <ul>
+                {products.slice(0, 10).map(product =>(
+                    <li key={product.id}>
+                        <img 
+                            src={product.thumbnail}
+                            alt={product.title}
+                        />
+
+                        <div>
+                            <strong>
+                                {product.title} - ${product.price} -
+                                 Sku: {product.sku} - Stock: {product.stock} - Description: {product.description}       
+                            </strong>
+                        </div>
+                        
+                        <div className='btn-buy'>
+                            <button onClick={handleChange}>
+                                Comprar
+                            </button>
+                        </div>
+
+                        <div className='' >
+                                <p className='mensagge-great'>{buy  ? "Gracias por su compra" : ""}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+}
