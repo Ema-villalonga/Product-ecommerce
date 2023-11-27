d406613dc7a40b450dd69992fd8e0b81a8b28283
Angel Emanuel Villalonga <88913610+Ema-villalonga@users.noreply.github.com>
2023-11-27 16:57:23 -0300

    Continue

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,34 +10,31 @@ export default function Products({products}){
     }
 
     return (
-        <main className='products'>
-            <ul>
-                {products.slice(0, 10).map(product =>(
-                    <li key={product.id}>
-                        <img 
-                            src={product.thumbnail}
-                            alt={product.title}
-                        />
+      <main className="products">
+        <ul>
+          {products.slice(0, 10).map((product) => (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
 
-                        <div>
-                            <strong>
-                                {product.title} - ${product.price} -
-                                 Sku: {product.sku} - Stock: {product.stock} - Description: {product.description}       
-                            </strong>
-                        </div>
-                        
-                        <div className='btn-buy'>
-                            <button onClick={handleChange}>
-                                Comprar
-                            </button>
-                        </div>
+              <div>
+                <strong>
+                  {product.title} - ${product.price} - Sku: {product.sku} -
+                  Stock: {product.stock} - Description: {product.description}
+                </strong>
+              </div>
 
-                        <div className='' >
-                                <p className='mensagge-great'>{buy  ? "Gracias por su compra" : ""}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    )
+              <div className="btn-buy">
+                <button onClick={handleChange}>Comprar</button>
+              </div>
+
+              <div className="">
+                <p className="mensagge-great">
+                  {buy ? "Gracias por su compra" : ""}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
 }
